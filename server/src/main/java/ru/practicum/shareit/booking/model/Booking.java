fce3cb7252129b5669fe0348e01670e7e736626c
Gl.Kurkin <gleee.kurk@gmail.com>
2025-07-09 11:04:29 +0300

    Исправлена ошибка в тестах

--- server/src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ server/src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -13,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
